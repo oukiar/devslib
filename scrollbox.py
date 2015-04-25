dcae8e3aeca656a224d138510d1c96b07dab91ea
oukiar <oukiar@gmail.com>
2015-04-25 18:53:30 -0500

    Scrollbox hotfix

--- scrollbox.py
+++ scrollbox.py
@@ -9,35 +9,39 @@ class ScrollBox(ScrollView):
     
     def __init__(self, **kwargs):
 
-    
+        orientation = kwargs.pop("orientation", "vertical")
+        
         super(ScrollBox, self).__init__(**kwargs)
-            
-        if self.orientation == 'vertical':
+        
+        if orientation == 'vertical':
             self.layout = BoxLayout(orientation='vertical', size_hint_y=None, spacing=10, padding=10)
             self.layout.height = 0
         else:
             self.layout = BoxLayout(size_hint_x=None, spacing=10, padding=10)
             self.layout.width = 0
     
-    
         super(ScrollBox, self).add_widget(self.layout)
         
     def add_widget(self, w, index=0):
         
-        self.layout.add_widget(w)
-        
         if self.orientation == 'vertical':
             
+            if len(self.layout.children) == 0:
+                self.layout.height = self.layout.padding[0]
+                
             self.layout.height += w.height + self.layout.spacing
-        
-            if len(self.children) == 1:
-                self.layout.height = self.layout.spacing
+            
         
         else:
-            self.layout.width += w.width + self.layout.spacing
+            if len(self.layout.children) == 0:
+                self.layout.width = self.layout.padding[0]
                 
-            if len(self.children) == 1:
-                self.layout.width = self.layout.spacing
+            self.layout.width += w.width + self.layout.spacing
+        
+        print self.layout.size
+        print self.orientation
+        
+        self.layout.add_widget(w, index)
                 
     def clear(self):
         self.layout.clear_widgets()
