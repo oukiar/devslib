bbdf24712d8932f817a4a6e10b28d67ef30dfb33
oukiar <oukiar@gmail.com>
2015-04-29 09:55:14 -0500

    boxlayout fix

--- scrollbox.py
+++ scrollbox.py
@@ -1,26 +1,36 @@
 
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, NumericProperty
 
 class ScrollBox(ScrollView):
-    
-    orientation = StringProperty()
+    orientation = StringProperty("default")
+    spacing = NumericProperty(10)
+    padding = NumericProperty(10)
     
     def __init__(self, **kwargs):
-
-        orientation = kwargs.pop("orientation", "vertical")
         
         super(ScrollBox, self).__init__(**kwargs)
         
-        if orientation == 'vertical':
-            self.layout = BoxLayout(orientation='vertical', size_hint_y=None, spacing=10, padding=10)
-            self.layout.height = 0
-        else:
-            self.layout = BoxLayout(size_hint_x=None, spacing=10, padding=10)
-            self.layout.width = 0
-    
+        self.layout = BoxLayout()   
+        self.layout.height = 0 
+        self.layout.width = 0
         super(ScrollBox, self).add_widget(self.layout)
+        
+        print "scrollbox initialized"
+        
+    def on_orientation(self, w, val):
+        self.layout.orientation = val
+        if val == 'vertical':
+            self.layout.size_hint_y=None
+        else:
+            self.layout.size_hint_x=None
+        
+    def on_spacing(self, w, val):
+        self.layout.spacing = val
+        
+    def on_padding(self, w, val):
+        self.layout.padding = val
         
     def add_widget(self, w, index=0):
         
