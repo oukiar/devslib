cfecd1d87fc1be1a229213e72e224ac0225aa5bf
oukiar <oukiar@gmail.com>
2016-01-24 17:19:57 -0600

    network hotfix

--- network.py
+++ network.py
@@ -789,10 +789,12 @@ class Network:
                 self.ngsock = None
                 
                 while self.ngsock == None:
-                    print "Creating socket: ", (ips[0], c)
+                    
                     if ips[0] != "127.0.0.1":
+                        print "Creating socket: ", (ips[0], c)
                         self.create_socket(ips[0], c, dispatcher)
                     else:
+                        print "Creating socket: ", (ips[1], c)
                         self.create_socket(ips[1], c, dispatcher)
 
                     c += 1
