1091ab89b61b1478a4d80b83365289ce12998fb6
Oscar Alcantara <oukiar@gmail.com>
2021-07-08 01:09:13 -0500

    agregada excepcion a network.py para evitar error al no tener conexion a internet

--- network.py
+++ network.py
@@ -767,9 +767,12 @@ class Network:
         '''
         
         
-        
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(("8.8.8.8", 80))
+        except:
+            return []
+
         ip = s.getsockname()[0]
         print("IP", str(ip))
         s.close()
